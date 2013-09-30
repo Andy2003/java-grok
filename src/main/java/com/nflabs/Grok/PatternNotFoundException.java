b52f2b96ede49724239a791f412e180d07e84ed7
Andreas Berger <aby@mms-dresden.de>
2013-09-30 14:52:54 +0200

    refactor 1

---
+++ src/main/java/com/nflabs/Grok/PatternNotFoundException.java
@@ -0,0 +1,16 @@
+package com.nflabs.Grok;
+
+/**
+ * User: abg
+ * Date: 30.09.13
+ * Time: 14:34
+ */
+public class PatternNotFoundException extends Exception {
+
+    public PatternNotFoundException() {
+    }
+
+    public PatternNotFoundException(String message) {
+        super(message);
+    }
+}
